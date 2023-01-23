08d0fe49114ab21a26b4523feb55a6285a2ce3d2
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2023-01-23 15:06:57 +0300

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js,ts,jsx,tsx}",
+    "./components/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  plugins: [],
 };
